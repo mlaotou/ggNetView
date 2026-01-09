92567356e85680d12653d86df22c3717578a2bd2
Jiawang1209 <Jiawang1209@163.com>
2026-01-09 23:22:55 +0800

    update

---
+++ R/VirtualPerturbation.R
@@ -0,0 +1,14 @@
+VirtualPerturbation <- function(mat,
+                                transfrom.method = c("none", "scale", "center", "log2", "log10", "ln", "rrarefy", "rrarefy_relative"),
+                                r.threshold = 0.7,
+                                p.threshold = 0.05,
+                                method = c("WGCNA", "SpiecEasi", "SPARCC", "cor"),
+                                cor.method = c("pearson", "kendall", "spearman"),
+                                proc = c("Bonferroni", "Holm", "Hochberg", "SidakSS", "SidakSD","BH", "BY","ABH","TSBH"),
+                                module.method = c("Fast_greedy", "Walktrap", "Edge_betweenness", "Spinglass"),
+                                SpiecEasi.method = c("mb", "glasso"),
+                                node_annotation = NULL,
+                                top_modules = 15,
+                                seed = 1115){
+
+}
